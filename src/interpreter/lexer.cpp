db2ecf8b984cef6f01d2338616f71e84871c3fa4
Richard Baltrusch <richard@baltrusch.net>
2021-11-14 11:47:55 +0100

    Add implementation of Lexer.split and Lexer.lex

--- src/interpreter/lexer.cpp
+++ src/interpreter/lexer.cpp
@@ -2,8 +2,47 @@
 Author: R. Baltrusch
 */
 
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "Token.hpp"
 #include "Lexer.hpp"
 #include "TokenFactory.hpp"
 
 Lexer::Lexer(TokenFactory &tokenFactory)
     : tokenFactory{tokenFactory} {};
+
+std::vector<std::shared_ptr<Token>> Lexer::lex(std::string &text)
+{
+    std::vector<std::shared_ptr<Token>> tokens;
+    std::vector<std::string> strings = this->split(text);
+    for ( const std::string& string : strings)
+    {
+        std::shared_ptr<Token> token = this->tokenFactory.create_token(string);
+        tokens.push_back(token);
+    }
+    return tokens;
+};
+
+std::vector<std::string> Lexer::split(std::string &text)
+{
+    std::vector<std::string> tokens;
+    std::string delim = " ";
+
+    auto start = 0U;
+    auto end = text.find(delim);
+    while (end != std::string::npos)
+    {
+        std::string element = text.substr(start, end - start);
+        tokens.push_back(element);
+
+        start = end + delim.length();
+        end = text.find(delim, start);
+    }
+
+    std::string lastElement = text.substr(start, end);
+    tokens.push_back(lastElement);
+
+    return tokens;
+};
