36c21b9effb701c55603e94a2751bf7be94aa2d3
Richard Baltrusch <richard@baltrusch.net>
2021-11-14 11:51:35 +0100

    Add implementation of TokenFactory.create_token

--- src/interpreter/tokenfactory.cpp
+++ src/interpreter/tokenfactory.cpp
@@ -2,9 +2,32 @@
 Author: R. Baltrusch
 */
 
+#include <map>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
 #include "Token.hpp"
 #include "TokenFactory.hpp"
 
 TokenFactory::TokenFactory(ConstructorMap tokens, ConstructorMap regexTokens)
     : tokens(tokens),
       regexTokens(regexTokens){};
+
+std::shared_ptr<Token> TokenFactory::create_token(const std::string& string)
+{
+    ConstructorMap::iterator iterator = this->tokens.find(string);
+    if (iterator != this->tokens.end())
+    {
+        std::any value = 0;
+        int line = 1;
+        std::shared_ptr<Token> token = iterator->second(value, line);
+        return token;
+    }
+
+    std::stringstream messageStream;
+    messageStream << "Invalid Token: " << string;
+    std::string message = messageStream.str();
+    throw std::invalid_argument(message);
+};
