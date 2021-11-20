4ee87d90d4657d756f1bbb4ecc6b172cb93c7931
Richard Baltrusch <richard@baltrusch.net>
2021-11-21 00:56:25 +0100

    Add implementation for Parser.parse

--- src/interpreter/implementation/Parser.cpp
+++ src/interpreter/implementation/Parser.cpp
@@ -3,7 +3,62 @@ Author: R. Baltrusch
 */
 
 #include <stack>
+#include <vector>
+#include <deque>
 
+#include "../headers/Token.hpp"
 #include "../headers/Parser.hpp"
 
 Parser::Parser() : tokenStack(){};
+
+std::vector<Token> Parser::parse(std::deque<std::shared_ptr<Token>> &tokens)
+{
+    std::vector<Token> parsedTokens;
+
+    while (!tokens.empty())
+    {
+        auto poppedTokens = tokens[0]->popTokens(tokens);
+        if (!poppedTokens.empty())
+        {
+            continue;
+        }
+
+        auto token = tokens[0];
+        tokens.pop_front();
+        if (this->tokenStack.empty())
+        {
+            tokenStack.push(*token);
+            continue;
+        }
+
+        if (!this->tokenStack.top().getSatisfied() || this->tokenStack.top().checkOptionalToken(*token))
+        {
+            this->tokenStack.top().addToken(*token);
+        }
+        else
+        {
+            parsedTokens.push_back(*token);
+        }
+        this->tokenStack.push(*token);
+
+        while (this->tokenStack.top().getFull())
+        {
+            auto token = this->tokenStack.top();
+            this->tokenStack.pop();
+            if (this->tokenStack.empty())
+            {
+                parsedTokens.push_back(token);
+                break;
+            }
+        }
+    }
+
+    while (!this->tokenStack.empty())
+    {
+        auto token = this->tokenStack.top();
+        this->tokenStack.pop();
+        parsedTokens.push_back(token);
+    }
+
+    return parsedTokens;
+};
