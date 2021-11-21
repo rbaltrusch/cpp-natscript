3983fcf030b9658719b81dbb4d4ee248742c92c4
Richard Baltrusch <richard@baltrusch.net>
2021-11-21 23:34:07 +0100

    Add constructors and getName methods for Value and Variable

--- src/interpreter/headers/Value.hpp
+++ src/interpreter/headers/Value.hpp
@@ -6,28 +6,37 @@ Author: R. Baltrusch
 #ifndef VALUE_H
 #define VALUE_H
 
+#include <any>
 #include <string>
+#include <stdexcept>
 
 class Value
 {
+public:
+    std::any value;
+
+    Value(std::any value)
+        : value{value} {};
+
+    virtual std::string getName(void)
+    {
+        throw std::runtime_error("Not implemented.");
+    };
 };
 
-class Integer : public Value
+class Variable : public Value
 {
-
-    int value;
-
-public:
-    Integer(int);
-};
-
-class Variable
-{
-
-public:
-    Variable(std::string);
-    Value value;
     std::string name;
+
+public:
+    Variable(const std::string &name)
+        : Value(0),
+          name{name} {};
+
+    std::string getName(void)
+    {
+        return this->name;
+    };
 };
 
 #endif
