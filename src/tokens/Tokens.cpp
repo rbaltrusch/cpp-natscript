b9f76f2a29765eb143a4878d2af32cc653d3b18e
Richard Baltrusch <richard@baltrusch.net>
2021-11-21 19:16:00 +0100

    Remove commented out code

--- src/tokens/Tokens.cpp
+++ src/tokens/Tokens.cpp
@@ -10,10 +10,6 @@ Author: R. Baltrusch
 #include "../interpreter/headers/Token.hpp"
 #include "Tokens.hpp"
 
-//template <class T>
-//Token create(std::any *value, int line) { return T(value, line); }
-//Token *(*createToken)(std::any *value, int line) = create<Token>(value, line);
-
 class TrueToken : public Token
 {
 public:
@@ -54,22 +50,17 @@ protected:
 
 ConstructorMap createTokensMap(void)
 {
-    //Token token = create<Token>(&value, 1);
-
-    //std::unique_ptr<Token> *(*createToken)(std::any * value, int line) = create<Token>(value, line);
     ConstructorMap tokens;
     tokens["this"] = create<Token>;
     tokens["is"] = create<Token>;
     tokens["a"] = create<Token>;
     tokens["true"] = create<TrueToken>;
     tokens["test"] = create<TestToken>;
-    //{{"a", &createToken}};
     return tokens;
 };
 
 ConstructorMap createRegexTokensMap(void)
 {
     ConstructorMap regexTokens;
-    //{{"a", &createToken}};
     return regexTokens;
 };
